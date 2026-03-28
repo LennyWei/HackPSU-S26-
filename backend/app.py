b8f88721831ff609de10591c1ca23335f0b3f84a
Roberto Ramirez <robertoramirezgalan04@gmail.com>
2026-03-28 14:22:09 -0400

    Docker setup

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/health")
+def health():
+    return {"status": "Backend alive!"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
